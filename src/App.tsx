cd133bb713aa68b640c2b6267662ddb34d8c5a3f
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-04-16 15:44:04 -0300

    feat: create login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import LoginPage from './components/Login/LoginPage'
 import PeopleListPage from './components/People/Page'
 import { Toaster } from './components/ui/sonner'
 
@@ -6,7 +7,8 @@ function App() {
 
   return (
     <>
-      <PeopleListPage />
+      <LoginPage />
+      {/* <PeopleListPage /> */}
       <Toaster />
     </>
   )
---
+++ src/components/Login/LoginPage.tsx
@@ -0,0 +1,15 @@
+"use client";
+import * as React from "react";
+import { BrandSection } from "./BrandSection";
+import { LoginForm } from "./LoginForm";
+
+export default function LoginPage() {
+  return (
+    <main className="flex overflow-hidden gap-10 justify-center items-center px-11 py-20 bg-zinc-50 max-md:px-5">
+      <section className="flex flex-wrap gap-10 justify-center items-center self-stretch py-8 pr-32 pl-8 my-auto bg-zinc-50 min-w-60 rounded-[54px] max-md:px-5 max-md:max-w-full">
+        <BrandSection />
+        <LoginForm />
+      </section>
+    </main>
+  );
+}
