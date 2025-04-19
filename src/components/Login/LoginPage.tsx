733d8f5c88b4437068886cf47bfe2071eed64508
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-04-19 12:54:05 -0300

    fix: space

--- src/components/Login/LoginPage.tsx
+++ src/components/Login/LoginPage.tsx
@@ -5,8 +5,8 @@ import { LoginForm } from "./LoginForm";
 
 export default function LoginPage() {
   return (
-    <main className="flex overflow-hidden gap-10 justify-center items-center px-11 py-20  max-md:px-5">
-      <section className="flex flex-wrap gap-10 justify-center xl:justify-between items-center self-stretch py-8 my-auto  min-w-60 rounded-[54px] max-md:px-5 max-md:max-w-full">
+    <main className="flex overflow-hidden min-h-screen justify-center items-center px-11 py-6 max-md:px-5">
+      <section className="flex flex-wrap gap-10 justify-center xl:justify-between items-center self-stretch my-auto min-w-60 max-md:px-5 max-md:max-w-full">
         <BrandSection />
         <LoginForm />
       </section>
