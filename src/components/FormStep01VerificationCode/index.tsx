b4c17134993eabf4e61b7b5d857be31223a32ef5
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-04-23 20:03:32 -0300

    fix: desktop and mobile diferences

--- src/components/FormStep01VerificationCode/index.tsx
+++ src/components/FormStep01VerificationCode/index.tsx
@@ -65,30 +65,30 @@ const VerificationCodeInput: React.FC = () => {
         className="object-cover absolute inset-0 size-full"
       />
 
-      <article className="flex relative z-0 flex-col justify-center p-[30px] my-auto rounded-2xl bg-background-dark min-w-60 w-fit max-md:max-w-full">
-        <div className="flex overflow-hidden z-0 justify-center items-center self-center bg-primary shadow aspect-[1/1] h-[41px] rounded-[999px] w-[41px]">
+      <article className="flex relative z-0 flex-col justify-center p-[30px] sm:p-10 gap-5 sm:gap-9 my-auto rounded-2xl bg-background-dark min-w-60 sm:min-w-md w-fit max-md:max-w-full">
+        <div className="flex overflow-hidden z-0 justify-center items-center self-center bg-primary shadow aspect-[1/1] h-[41px] w-[41px] sm:h-[77px] sm:w-[77px] rounded-full">
           <img
             src="https://cdn.builder.io/api/v1/image/assets/TEMP/2f78f74fe625cfd2f07e723367ef47a2cdf15fc6?placeholderIfAbsent=true"
             alt="Lock icon"
-            className="object-contain self-stretch my-auto aspect-square stroke-[2px] stroke-background w-4"
+            className="object-contain self-stretch my-auto aspect-square stroke-[2px] stroke-background w-4 sm:w-[34px]"
           />
         </div>
 
         <header className="flex z-0 mt-5 flex-col w-full text-center">
-          <h1 className="text-sm font-semibold text-grey-darker">
+          <h1 className="text-sm font-semibold sm:text-3xl sm:font-bold text-grey-darker">
             Digite o código e <br />
             clique em{" "}
             <span className="text-primary">"Confirmar"<br /></span>
             para avançar
           </h1>
-          <p className="self-center mt-2.5 text-xs text-grey-light w-[289px]">
+          <p className="self-center mt-2.5 text-xs sm:text-base sm:font-medium text-grey-light w-[289px]">
             Ou clique em "Reenviar"
             <br />
             para receber o código por email
           </p>
         </header>
 
-        <div className="flex z-0 gap-1.5 items-start self-center p-2.5 mt-9 text-center whitespace-nowrap">
+        <div className="flex z-0 gap-1.5 sm:gap-2.5 items-start self-center p-2.5 mt-9 text-center whitespace-nowrap">
           {[...Array(codeLength)].map((_, index) => (
             <input
               key={index}
@@ -101,20 +101,20 @@ const VerificationCodeInput: React.FC = () => {
               value={code[index] || ""}
               placeholder={getPlaceholder(index)}
               onChange={(e) => handleInputChange(e, index)}
-              className="self-stretch px-3 py-3 rounded-xl shadow bg-background min-h-[59px] w-[46px] flex justify-center items-center text-center text-3xl font-medium text-grey-darker"
+              className="self-stretch px-3 py-3 rounded-md sm:rounded-xl shadow bg-background h-10 sm:min-h-[59px] w-[31px] sm:w-[46px] flex justify-center items-center text-center text-sm sm:text-3xl font-medium text-grey-darker"
               inputMode="numeric"
             />
           ))}
         </div>
 
         <div className="flex z-0 gap-5 items-center justify-between mt-9 w-full">
-          <Button variant="secondary" className="bg-grey-light/10 h-9 rounded-lg font-semibold text-xs shadow-sm" onClick={handleResend}>Reenviar</Button>
-          <Button className="h-9 rounded-lg font-semibold text-xs" onClick={handleConfirm}>Confirmar</Button>
+          <Button variant="secondary" className="bg-grey-light/10 h-9 sm:h-14 rounded-lg sm:rounded-xl font-semibold text-xs sm:text-xl shadow-sm" onClick={handleResend}>Reenviar</Button>
+          <Button className="h-9 sm:h-14 rounded-lg sm:rounded-xl font-semibold text-xs sm:text-xl" onClick={handleConfirm}>Confirmar</Button>
         </div>
 
         <button
           aria-label="Close"
-          className="absolute cursor-pointer top-5 right-5 z-0 px-3.5 w-8 h-8 rounded-sm bg-grey-light/10 bg-opacity-30 min-h-8 flex items-center justify-center flex-col"
+          className="absolute cursor-pointer top-5 right-5 z-0 px-3.5 w-8 h-8 sm:w-11 sm:h-11 rounded-sm bg-grey-light/10 bg-opacity-30 min-h-8 flex items-center justify-center flex-col"
           onClick={handleClose}
         >
           <X className="text-grey-dark" />
