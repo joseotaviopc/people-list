255103c0fbf617fd6a0738b4647bccc716723752
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-05-02 21:09:33 -0300

    fix: reactivate validation in forms

--- src/components/FormStep03/types.ts
+++ src/components/FormStep03/types.ts
@@ -1,3 +1,4 @@
+import { validateCPF, validatePhone } from "@/helpers";
 import { z } from "zod";
 
 export enum FormType {
@@ -10,128 +11,120 @@ export enum FormType {
 // SOCIO
 export const socioSchema = z.object({
     name: z.string().min(2, "Nome deve ter pelo menos 2 caracteres"),
-    email: z.string(),
-    // .email("Deve ser um email válido"),
-    cpf: z.string(),
-    // .min(14, "CPF deve ter 11 dígitos")
-    // .transform((cpf) => cpf.replace(/[.|-]/g, ''))
-    // .refine((cpf) => validateCPF(cpf), "CPF inválido"),
-    celphone: z.string(),
-    // .min(15, "Telefone deve ter 11 dígitos")
-    // .transform((phone) => phone.replace(/[(|)|\-|\s]/g, ''))
-    // .refine((phone) => validatePhone(phone), "Telefone inválido"),
+    email: z.string().email("Deve ser um email válido"),
+    cpf: z.string()
+        .min(14, "CPF deve ter 11 dígitos")
+        .transform((cpf) => cpf.replace(/[.|-]/g, ''))
+        .refine((cpf) => validateCPF(cpf), "CPF inválido"),
+    celphone: z.string()
+        .min(15, "Telefone deve ter 11 dígitos")
+        .transform((phone) => phone.replace(/[(|)|\-|\s]/g, ''))
+        .refine((phone) => validatePhone(phone), "Telefone inválido"),
 });
 export type SocioFormData = z.infer<typeof socioSchema>;
 
 // MULHER
 export const mulherSchema = z.object({
     name: z.string().min(2, "Nome deve ter pelo menos 2 caracteres"),
-    email: z.string(),
-    // .email("Deve ser um email válido"),
-    cpf: z.string(),
-    // .min(14, "CPF deve ter 11 dígitos")
-    // .transform((cpf) => cpf.replace(/[.|-]/g, ''))
-    // .refine((cpf) => validateCPF(cpf), "CPF inválido"),
-    celphone: z.string(),
-    // .min(15, "Telefone deve ter 11 dígitos")
-    // .transform((phone) => phone.replace(/[(|)|\-|\s]/g, ''))
-    // .refine((phone) => validatePhone(phone), "Telefone inválido"),
+    email: z.string().email("Deve ser um email válido"),
+    cpf: z.string()
+        .min(14, "CPF deve ter 11 dígitos")
+        .transform((cpf) => cpf.replace(/[.|-]/g, ''))
+        .refine((cpf) => validateCPF(cpf), "CPF inválido"),
+    celphone: z.string()
+        .min(15, "Telefone deve ter 11 dígitos")
+        .transform((phone) => phone.replace(/[(|)|\-|\s]/g, ''))
+        .refine((phone) => validatePhone(phone), "Telefone inválido"),
 });
 export type MulherFormData = z.infer<typeof mulherSchema>;
 
 // FILHO
 export const filhoSchema = z.object({
     name: z.string().min(2, "Nome deve ter pelo menos 2 caracteres"),
-    email: z.string(),
-    // .email("Deve ser um email válido"),
-    cpf: z.string(),
-    // .min(14, "CPF deve ter 11 dígitos")
-    // .transform((cpf) => cpf.replace(/[.|-]/g, ''))
-    // .refine((cpf) => validateCPF(cpf), "CPF inválido"),
-    celphone: z.string(),
-    // .min(15, "Telefone deve ter 11 dígitos")
-    // .transform((phone) => phone.replace(/[(|)|\-|\s]/g, ''))
-    // .refine((phone) => validatePhone(phone), "Telefone inválido"),
+    email: z.string().email("Deve ser um email válido"),
+    cpf: z.string()
+        .min(14, "CPF deve ter 11 dígitos")
+        .transform((cpf) => cpf.replace(/[.|-]/g, ''))
+        .refine((cpf) => validateCPF(cpf), "CPF inválido"),
+    celphone: z.string()
+        .min(15, "Telefone deve ter 11 dígitos")
+        .transform((phone) => phone.replace(/[(|)|\-|\s]/g, ''))
+        .refine((phone) => validatePhone(phone), "Telefone inválido"),
 });
 export type FilhoFormData = z.infer<typeof filhoSchema>;
 
 // MULHERFILHO
 export const mulherDoFilhoSchema = z.object({
     name: z.string().min(2, "Nome deve ter pelo menos 2 caracteres"),
-    email: z.string(),
-    // .email("Deve ser um email válido"),
-    cpf: z.string(),
-    // .min(14, "CPF deve ter 11 dígitos")
-    // .transform((cpf) => cpf.replace(/[.|-]/g, ''))
-    // .refine((cpf) => validateCPF(cpf), "CPF inválido"),
-    celphone: z.string(),
-    // .min(15, "Telefone deve ter 11 dígitos")
-    // .transform((phone) => phone.replace(/[(|)|\-|\s]/g, ''))
-    // .refine((phone) => validatePhone(phone), "Telefone inválido"),
+    email: z.string().email("Deve ser um email válido"),
+    cpf: z.string()
+        .min(14, "CPF deve ter 11 dígitos")
+        .transform((cpf) => cpf.replace(/[.|-]/g, ''))
+        .refine((cpf) => validateCPF(cpf), "CPF inválido"),
+    celphone: z.string()
+        .min(15, "Telefone deve ter 11 dígitos")
+        .transform((phone) => phone.replace(/[(|)|\-|\s]/g, ''))
+        .refine((phone) => validatePhone(phone), "Telefone inválido"),
 });
 export type MulherDoFilhoFormData = z.infer<typeof mulherDoFilhoSchema>;
 
 // NETO
 export const netoSchema = z.object({
     name: z.string().min(2, "Nome deve ter pelo menos 2 caracteres"),
-    email: z.string(),
-    // .email("Deve ser um email válido"),
-    cpf: z.string(),
-    // .min(14, "CPF deve ter 11 dígitos")
-    // .transform((cpf) => cpf.replace(/[.|-]/g, ''))
-    // .refine((cpf) => validateCPF(cpf), "CPF inválido"),
-    celphone: z.string(),
-    // .min(15, "Telefone deve ter 11 dígitos")
-    // .transform((phone) => phone.replace(/[(|)|\-|\s]/g, ''))
-    // .refine((phone) => validatePhone(phone), "Telefone inválido"),
+    email: z.string().email("Deve ser um email válido"),
+    cpf: z.string()
+        .min(14, "CPF deve ter 11 dígitos")
+        .transform((cpf) => cpf.replace(/[.|-]/g, ''))
+        .refine((cpf) => validateCPF(cpf), "CPF inválido"),
+    celphone: z.string()
+        .min(15, "Telefone deve ter 11 dígitos")
+        .transform((phone) => phone.replace(/[(|)|\-|\s]/g, ''))
+        .refine((phone) => validatePhone(phone), "Telefone inválido"),
 });
 export type NetoFormData = z.infer<typeof netoSchema>;
 
 // MULHERNETO
 export const mulherDoNetoSchema = z.object({
     name: z.string().min(2, "Nome deve ter pelo menos 2 caracteres"),
-    email: z.string(),
-    // .email("Deve ser um email válido"),
-    cpf: z.string(),
-    // .min(14, "CPF deve ter 11 dígitos")
-    // .transform((cpf) => cpf.replace(/[.|-]/g, ''))
-    // .refine((cpf) => validateCPF(cpf), "CPF inválido"),
-    celphone: z.string(),
-    // .min(15, "Telefone deve ter 11 dígitos")
-    // .transform((phone) => phone.replace(/[(|)|\-|\s]/g, ''))
-    // .refine((phone) => validatePhone(phone), "Telefone inválido"),
+    email: z.string().email("Deve ser um email válido"),
+    cpf: z.string()
+        .min(14, "CPF deve ter 11 dígitos")
+        .transform((cpf) => cpf.replace(/[.|-]/g, ''))
+        .refine((cpf) => validateCPF(cpf), "CPF inválido"),
+    celphone: z.string()
+        .min(15, "Telefone deve ter 11 dígitos")
+        .transform((phone) => phone.replace(/[(|)|\-|\s]/g, ''))
+        .refine((phone) => validatePhone(phone), "Telefone inválido"),
 });
 export type MulherDoNetoFormData = z.infer<typeof mulherDoNetoSchema>;
 
 // BISNETO
 export const bisnetoSchema = z.object({
     name: z.string().min(2, "Nome deve ter pelo menos 2 caracteres"),
-    email: z.string(),
-    // .email("Deve ser um email válido"),
-    cpf: z.string(),
-    // .min(14, "CPF deve ter 11 dígitos")
-    // .transform((cpf) => cpf.replace(/[.|-]/g, ''))
-    // .refine((cpf) => validateCPF(cpf), "CPF inválido"),
-    celphone: z.string(),
-    // .min(15, "Telefone deve ter 11 dígitos")
-    // .transform((phone) => phone.replace(/[(|)|\-|\s]/g, ''))
-    // .refine((phone) => validatePhone(phone), "Telefone inválido"),
+    email: z.string().email("Deve ser um email válido"),
+    cpf: z.string()
+        .min(14, "CPF deve ter 11 dígitos")
+        .transform((cpf) => cpf.replace(/[.|-]/g, ''))
+        .refine((cpf) => validateCPF(cpf), "CPF inválido"),
+    celphone: z.string()
+        .min(15, "Telefone deve ter 11 dígitos")
+        .transform((phone) => phone.replace(/[(|)|\-|\s]/g, ''))
+        .refine((phone) => validatePhone(phone), "Telefone inválido"),
 });
 export type BisnetoFormData = z.infer<typeof bisnetoSchema>;
 
 // LEGAL REPRESENTATIVE
 const legalSchema = z.object({
     name: z.string().min(2, "Nome deve ter pelo menos 2 caracteres"),
-    email: z.string(),
-    // .email("Deve ser um email válido"),
-    cpf: z.string(),
-    // .min(14, "CPF deve ter 11 dígitos")
-    // .transform((cpf) => cpf.replace(/[.|-]/g, ''))
-    // .refine((cpf) => validateCPF(cpf), "CPF inválido"),
-    celphone: z.string(),
-    // .min(15, "Telefone deve ter 11 dígitos")
-    // .transform((phone) => phone.replace(/[(|)|\-|\s]/g, ''))
-    // .refine((phone) => validatePhone(phone), "Telefone inválido"),
+    email: z.string().email("Deve ser um email válido"),
+    cpf: z.string()
+        .min(14, "CPF deve ter 11 dígitos")
+        .transform((cpf) => cpf.replace(/[.|-]/g, ''))
+        .refine((cpf) => validateCPF(cpf), "CPF inválido"),
+    celphone: z.string()
+        .min(15, "Telefone deve ter 11 dígitos")
+        .transform((phone) => phone.replace(/[(|)|\-|\s]/g, ''))
+        .refine((phone) => validatePhone(phone), "Telefone inválido"),
 });
 export type LegalFormData = z.infer<typeof legalSchema>;
 
